0ba20e55a0f064cd16e32c4cd1905859d24ed1fa
mujiburrohman <mujibnewbie5@gmail.com>
2018-10-26 11:18:40 +0700

    Initial commit

---
+++ app/src/main/java/com/example/mozeeb/audioplayer/IntroActivity.java
@@ -0,0 +1,20 @@
+package com.example.mozeeb.audioplayer;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+    }
+    public void btn_in(View view){
+        Intent ti = new Intent(IntroActivity.this, MainActivity.class);
+        startActivity(ti);
+    }
+}
